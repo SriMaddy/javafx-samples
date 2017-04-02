f92d7d1e691a669a31bba3466477c037bf95abb9
Sridhar <sri.bharath72@gmail.com>
2017-04-02 19:12:04 +0530

    label added

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,25 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			FlowPane root = new FlowPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Test");
 			primaryStage.setScene(scene);
+			
+			Label label1 = new Label("Test Label");
+			root.getChildren().add(label1);
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
